3386b2fc41d1f834ef8315c1948a3f3e5d9626e7
Joan Alonso <jmalonsos@outlook.com>
2016-12-03 14:26:49 -0600

    Add common friends and rename previous function

--- lib/twinder/followers.ex
+++ lib/twinder/followers.ex
@@ -1,30 +1,37 @@
 defmodule Twinder.Followers do
 
-  def followers_for(username1, username2) do
-    friends_for_u1 = find_friends_for(username1)
-    friends_for_u2 = find_friends_for(username2)
-    followers_of_u1 = find_followers(username1)
-    followers_of_u2 = find_followers(username2)
+  def common_followers(username1, username2) do
+    friends_for_u1 = find_friends(username1)
+    friends_for_u2 = find_friends(username2)
+    followers_of_u1 = find_followers_for(username1)
+    followers_of_u2 = find_followers_for(username2)
     #following_for_u1 = for f <- friends_u1, f.following == true, do: f
     #following_for_u2 = for f <- friends_u2, f.following == true, do: f
-    {friends_for_u1, followers_of_u1, friends_for_u2, followers_of_u2}
+    bff1 = for friend_id <- friends_for_u1, follower <- followers_of_u1, friend_id == follower.id, do: follower
+    bff2 = for friend_id <- friends_for_u2, follower <- followers_of_u2, friend_id == follower.id, do: follower
+
+    common_followers = for follower_u1 <- followers_of_u1, follower_u2 <- followers_of_u2, follower_u1.id == follower_u2, do: follower_u1
   end
 
-  def find_friends_for(username) do
-    join_friends(username, [],-1)
+  def common_bff(username1, username2) do
+    #matches = for bff_of_u1 <- bff1, bff_of_u2 <- bff2, bff_of_u1.id == bff_of_u2, do: bff_of_u1
   end
 
-  def join_friends(_,friends, 0) do
-    friends
+  def find_followers_for(username) do
+    join_followers(username, [],-1)
   end
 
-  def join_friends(username,friends, cursor) do
-    current_page_friends = ExTwitter.friends(username, count: 200, cursor: cursor)
-    join_friends(username, friends ++ current_page_friends.items, current_page_friends.next_cursor)
+  def join_followers(_,followers, 0) do
+    followers
   end
 
-  def find_followers(username) do
-    ExTwitter.follower_ids(username)
+  def join_followers(username,followers, cursor) do
+    current_page_followers = ExTwitter.followers(username, count: 200, cursor: cursor)
+    join_followers(username, followers ++ current_page_followers.items, current_page_followers.next_cursor)
+  end
+
+  def find_friends(username) do
+    ExTwitter.friend_ids(username).items
   end
 
 end
