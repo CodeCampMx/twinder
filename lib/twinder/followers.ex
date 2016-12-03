0131b61e0a0d9463bc452c4014f7f43f7a6e4f6b
neodevelop <juan@makingdevs.com>
2016-12-03 14:41:40 -0600

    Crossing and inspecting

--- lib/twinder/followers.ex
+++ lib/twinder/followers.ex
@@ -5,16 +5,34 @@ defmodule Twinder.Followers do
     friends_for_u2 = find_friends(username2)
     followers_of_u1 = find_followers_for(username1)
     followers_of_u2 = find_followers_for(username2)
+    IO.inspect friends_for_u1
+    IO.inspect friends_for_u2
+    IO.inspect followers_of_u1
+    IO.inspect followers_of_u2
     #following_for_u1 = for f <- friends_u1, f.following == true, do: f
     #following_for_u2 = for f <- friends_u2, f.following == true, do: f
-    bff1 = for friend_id <- friends_for_u1, follower <- followers_of_u1, friend_id == follower.id, do: follower
-    bff2 = for friend_id <- friends_for_u2, follower <- followers_of_u2, friend_id == follower.id, do: follower
+    bff1 = for friend_id <- friends_for_u1,
+               follower <- followers_of_u1,
+               friend_id == follower.id,
+               do: follower
+    bff2 = for friend_id <- friends_for_u2,
+               follower <- followers_of_u2,
+               friend_id == follower.id,
+               do: follower
 
-    common_followers = for follower_u1 <- followers_of_u1, follower_u2 <- followers_of_u2, follower_u1.id == follower_u2, do: follower_u1
+    common_followers = for follower_u1 <- followers_of_u1,
+                           follower_u2 <- followers_of_u2,
+                           follower_u1.id == follower_u2,
+                           do: follower_u1
+
+    IO.puts common_followers
+    common_bff = common_bff(bff1,bff2)
+    IO.puts common_bff
+    {common_followers, common_bff}
   end
 
-  def common_bff(username1, username2) do
-    #matches = for bff_of_u1 <- bff1, bff_of_u2 <- bff2, bff_of_u1.id == bff_of_u2, do: bff_of_u1
+  def common_bff(bff1, bff2) do
+    for bff_of_u1 <- bff1, bff_of_u2 <- bff2, bff_of_u1.id == bff_of_u2, do: bff_of_u1
   end
 
   def find_followers_for(username) do
